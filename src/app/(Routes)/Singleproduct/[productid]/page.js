22b8411fdb012dd934743a5e25aba6af77149c49
IRS-IT-Service <irsitservice@gmail.com>
2024-05-20 16:55:37 +0530

    hello

---
+++ src/app/(Routes)/Singleproduct/[productid]/page.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const page = ({params}) => {
+    const id = params.productid
+  return (
+    <main>
+<div className="flex flex-col">
+
+        
+    </div>
+    </main>
+  )
+}
+
+export default page
